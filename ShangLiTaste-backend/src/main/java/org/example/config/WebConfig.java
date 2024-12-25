cd7539d20abf1cb0207e8e67b1d92ce90c96433e
xiaohan <bornwarm@foxmail.com>
2024-12-25 22:02:52 +0800

    Modify WebConfig.

--- ShangLiTaste-backend/src/main/java/org/example/config/WebConfig.java
+++ ShangLiTaste-backend/src/main/java/org/example/config/WebConfig.java
@@ -21,10 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 允许所有路径的跨域请求
-                .allowedOrigins("*") // 允许所有来源
+//                .allowedOrigins("*") // 允许所有来源
+                .allowedOriginPatterns("*") // 允许所有来源
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
                 .allowedHeaders("*") // 允许的请求头
-//                .allowCredentials(true) // 是否允许带认证信息
+                .allowCredentials(true) // 是否允许带认证信息
                 .maxAge(3600); // 预检请求的缓存时间
     }
 }
