bbbbfed2d9a8cfd3b3ea1359f94fbd6363f8e223
xiaohan <bornwarm@foxmail.com>
2024-12-25 16:32:11 +0800

    No login interception and global error.

--- ShangLiTaste-backend/src/main/java/org/example/config/WebConfig.java
+++ ShangLiTaste-backend/src/main/java/org/example/config/WebConfig.java
@@ -1,11 +1,22 @@
 package org.example.config;
 
+import org.example.interceptor.LoginCheckInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginCheckInterceptor loginCheckInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**").excludePathPatterns("/userlogin","/customerlogin","/userregister","/customerregister");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
